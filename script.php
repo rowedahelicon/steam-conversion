6480fdc6e9f54ff6757157f5e3f6693d08e3409f
Rowedahelicon <matthewr01@gmail.com>
2015-03-15 17:42:50 -0400

    Update script.php
    Now has support for Steam3 ids, along with a better / faster method of getting the steam 64 from a vanity url name. Includes a hour long cache timer for json files.

--- script.php
+++ script.php
@@ -1,22 +1,102 @@
+<?
+
+//EDIT THIS
+$api_key = "";
+//DON'T EDIT THE REST
+
+function steam3($steam2) {
+    $id = explode(':', $steam2);
+    $id3 = ($id[2] * 2) + $id[1];
+    return '[U:1:' . $id3 . ']';
+}
+
 function steamGrabber($steam_id)
 {
-//First we check what kind of ID it is
+
+$steam_id = htmlspecialchars($steam_id,ENT_NOQUOTES);
+$steam_id = preg_replace('/\s+/', '', $steam_id);
+
+//First let's find what kind of STEAM ID it is.
+
 $id_result = substr($steam_id, 0,4);
 switch($id_result){
+
+//Scenario 1 : Using a Steam 32 Bit ID
+
 case STEA:
 $split = explode(":", $steam_id); // STEAM_?:?:??????? format
-
 $x = substr($split[0], 6, 1);
 $y = $split[1];
 $z = $split[2];
-
 $result = ($z * 2) + 0x0110000100000000 + $y;
 break;
+
+//Scenario 2 : Using a Steam 64 Bit ID
+
 case 7656:
-$steam_id = ($steam_id - 76561197960265728) / 2;
-if(substr($steam_id, -2) == ".5"){ $steam_id = round($steam_id, 0, PHP_ROUND_HALF_DOWN); $result = "STEAM_0:1:$steam_id"; }else{ $result = "STEAM_0:0:$steam_id"; }
+$result = $steam_id;
 break;
+
+//Scenario 3 : Using a vanity URL
+
+case http:
+$find_1 = strpos("$steam_id", "//steamcommunity.com/id/"); //Math mode
+$find_2 = strpos("$steam_id", "//steamcommunity.com/profiles/"); //Easy mode
+
+if($find_1 >= 1){ 
+
+$xmlstring = "$steam_id?xml=1";
+$xml = simplexml_load_file($xmlstring);
+$json = json_encode($xml);
+$array = json_decode($json,TRUE);
+
+$result = $array[steamID64];
+
+}
+if($find_2 >= 1){ $result = (explode('/',$steam_id));
+$result = $result[4];
+
+ }
+break;
+
+//Scenario 4 : Using the Steam3 ID
+
+case "U:1:":
+//echo $steam_id;
+$steam_id = explode(":", $steam_id);
+$result = $steam_id[2] + 0x0110000100000000;
+
+break;
+
+//End Scenario : Attempt to get it from steam community directly or no possible retrieval 
+
 default:
+
+//Using a vanity name will require some extra work, and to help on performance, you can cache the json bits of need be!
+
+define("MAX_CACHE_LIFETIME", 60 * 60); //1 hour
+
+$localJSONCache = "resolve_$steam_id.json.cache";
+
+  $steam_profile_vanity = null;
+    if (file_exists($localJSONCache)) {
+        if (time() - filemtime($localJSONCache) < MAX_CACHE_LIFETIME) {
+            $steam_profile_vanity = @file_get_contents($localJSONCache);
+        }
+    }
+    if (empty($steam_profile_vanity)) {
+	$steam_profile_info=@file_get_contents("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=$api_key&vanityurl=$steam_id");
+	file_put_contents($localJSONCache, $steam_profile_vanity);
+	}
+	
+	$json_groups=json_decode($steam_group_info);
+	foreach($json_groups->response as $key){
+	if($key->message == "No match"){ $result = null; }else{ $result = $key->steamid; } //A null result occurs when there is no steam profile associated with the ID provided.
+	}
+
+
 }
 return $result;
 }
+
+?>
